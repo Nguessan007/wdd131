930c14eb78e4a631265c659e226ff3382f0eef38
Nguessan007 <enguessan@byupathway.edu>
2025-09-20 23:14:30 +0000

    k

---
+++ scripts/place.js
@@ -0,0 +1,17 @@
+// Footer Date
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Weather Calculation
+const temp = parseFloat(document.getElementById("temp").textContent);
+const wind = parseFloat(document.getElementById("wind").textContent);
+
+function calculateWindChill(t, s) {
+  return (13.12 + 0.6215*t - 11.37*Math.pow(s,0.16) + 0.3965*t*Math.pow(s,0.16)).toFixed(1);
+}
+
+if (temp <= 10 && wind > 4.8) {
+  document.getElementById("windchill").textContent = calculateWindChill(temp, wind) + " °C";
+} else {
+  document.getElementById("windchill").textContent = "N/A";
+}
